0418df9a66adb81d2c7371acbd5b559f8828ad1c
Your Name <you@example.com>
2023-08-22 20:29:16 +0800

    补充笔记

--- src/main/java/com/example/netty/codec/NettyClient.java
+++ src/main/java/com/example/netty/codec/NettyClient.java
@@ -15,11 +15,15 @@ public class NettyClient {
     public static void main(String[] args) {
         NioEventLoopGroup group = null;
         try {
+            // 创建一个group
             group = new NioEventLoopGroup();
-
+            // 创建一个Bootstrap
             Bootstrap bootstrap = new Bootstrap();
+            // - Bootstrap配置group
             bootstrap.group(group)
+                    // - Bootstrap配置NioSocketChannel类型
                     .channel(NioSocketChannel.class)
+                    // - Bootstrap往ChannelPipeline中添加ChannelHandler
                     .handler(new ChannelInitializer<SocketChannel>() {
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ChannelPipeline pipeline = ch.pipeline();
@@ -28,7 +32,9 @@ public class NettyClient {
                         }
                     });
             System.out.println("客户端 ok...");
+            // - Bootstrap绑定远程地址并使用ChannelFuture进行监听
             ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6668).sync();
+            // - 使用ChannelFuture监听关闭通道事件
             channelFuture.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
--- src/main/java/com/example/netty/codec/NettyServer.java
+++ src/main/java/com/example/netty/codec/NettyServer.java
@@ -11,19 +11,25 @@ import io.netty.handler.codec.protobuf.ProtobufEncoder;
 public class NettyServer {
 
     public static void main(String[] args) {
+        // 创建一个bossGroup
         EventLoopGroup bossGroup = null;
+        // 创建一个workerGroup
         EventLoopGroup workerGroup = null;
         try {
             // 默认线程数：cpu核数*2
             bossGroup = new NioEventLoopGroup(1);
             workerGroup = new NioEventLoopGroup(8);
-
+            // 创建一个ServerBootstrap
             ServerBootstrap bootstrap = new ServerBootstrap();
+            // ServerBootstrap配置bossGroup、workerGroup
             bootstrap.group(bossGroup, workerGroup)
+                    // ServerBootstrap配置NioServerSocketChannel类型
                     .channel(NioServerSocketChannel.class)
+                    // ServerBootstrap添加一些Channel配置
                     .option(ChannelOption.SO_BACKLOG, 128)
                     .option(ChannelOption.SO_KEEPALIVE, true)
                     // .handler(null) // handler 对应 bossGroup；childHandler 对应 workerGroup
+                    // ServerBootstrap添加Handler
                     .childHandler(new ChannelInitializer<SocketChannel>() { // 创建一个管道测试对象
                         // 给pipeline设置处理器
                         protected void initChannel(SocketChannel ch) throws Exception {
