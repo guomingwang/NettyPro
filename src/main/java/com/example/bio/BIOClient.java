937ffde446ea6ba0d0be7b67208f776d75d58527
Your Name <you@example.com>
2023-08-22 16:38:59 +0800

    补充笔记

--- src/main/java/com/example/bio/BIOClient.java
+++ src/main/java/com/example/bio/BIOClient.java
@@ -2,6 +2,7 @@ package com.example.bio;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 /**
@@ -11,17 +12,17 @@ import java.util.Scanner;
 public class BIOClient {
 
     public static void main(String[] args) throws IOException {
-        // Socket
+        // 创建一个客户端Socket
         Socket socket = new Socket("127.0.0.1", 6666);
-        // 输出流
+        // 从客户端socket获取输出流
         OutputStream outputStream = socket.getOutputStream();
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
-        PrintStream printStream = new PrintStream(bufferedOutputStream, true, "UTF-8");
-        // 输入流
+        // 创建一个扫描流
         Scanner scanner = new Scanner(System.in);
+        // 扫描流循环读取键盘输入数据，填充到输出流
         while (true) {
             while (scanner.hasNextLine()) {
-                printStream.println(scanner.nextLine());
+                String line = scanner.nextLine();
+                outputStream.write(line.getBytes(StandardCharsets.UTF_8));
             }
         }
     }
--- src/main/java/com/example/bio/BIOServer.java
+++ src/main/java/com/example/bio/BIOServer.java
@@ -4,42 +4,46 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class BIOServer {
 
     public static void main(String[] args) throws IOException {
-        // 线程池
+        // 创建一个线程池：核心线程数0，非核心线程数Integer的最大值，空闲等待时间60s
+        // return new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+        //                                      60L, TimeUnit.SECONDS,
+        //                                      new SynchronousQueue<Runnable>());
         ExecutorService newCachedThreadPool = Executors.newCachedThreadPool();
-        // ServerSocket
+        // 创建一个ServerSocket
         ServerSocket serverSocket = new ServerSocket(6666);
         System.out.println("服务器启动了");
+        // 循环监听连接
         while (true) {
             // 主线程
             System.out.println("主线程线程信息 id=" + Thread.currentThread().getId() + "名字=" + Thread.currentThread().getName());
             System.out.println("等待连接...");
+            // 获取建立连接的客户端socket
             final Socket socket = serverSocket.accept();
             System.out.println("连接到一个客户端");
-            newCachedThreadPool.execute(new Runnable() {
-                public void run() {
-                    handler(socket);
-                }
-            });
+            // 从线程池中执行一个线程任务
+            newCachedThreadPool.execute(() -> handler(socket));
         }
     }
 
-    public static void handler(Socket socket) {
+    private static void handler(Socket socket) {
         try {
-            // 子线程
             System.out.println("子线程线程信息 id=" + Thread.currentThread().getId() + "名字=" + Thread.currentThread().getName());
+            // 从客户端socket获取输入流
             InputStream inputStream = socket.getInputStream();
+            // 从输入流读取数据并打印（最终目的）
             byte[] bytes = new byte[1024];
             while (true) {
                 System.out.println("read...");
                 int read = inputStream.read(bytes);
                 if (read != -1) {
-                    System.out.println(new String(bytes, 0, read));
+                    System.out.println(new String(bytes, 0, read, StandardCharsets.UTF_8));
                 } else {
                     break;
                 }
@@ -49,6 +53,7 @@ public class BIOServer {
         } finally {
             System.out.println("关闭和client的连接");
             try {
+                // 关闭客户端socket
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
